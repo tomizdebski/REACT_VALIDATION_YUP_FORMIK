d93230ad86a814d657ffaa38821e4a37d1c4309a
tomizdebski <tomizdebski@gmail.com>
2023-06-24 15:05:42 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,42 +1,136 @@
-import React from 'react';
+import { Formik, Field } from "formik";
 import {
-  ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+  Button,
+  Select,
+  Flex,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  VStack
+} from "@chakra-ui/react";
+import * as yup from "yup"
 
-function App() {
+const validationSchema=yup.object().shape({
+  name: yup.string('To polemusi być napisem').required('To pole jest wymagane').min(5 | "To p[ole musi mieć 5 liter"), 
+  age: yup.number('To pole musi byc numerem').min(18).max(99),
+
+});
+ 
+export default function App() {
+  
+
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
-  );
-}
+    <Flex bg="gray.100" align="center" justify="center" h="100vh">
+        <Box bg="white" p={6} rounded="md" w={64}>
+          <Formik
+            initialValues={{
+              name: "",
+              age: 0,
+              city: "",
+              volunteer: false,
+              photo: "",
+              zip:"",
+              telephone:""
+            }}
+            validationSchema={validationSchema} // podpinamy schemat walidacji
+            onSubmit={(values) => {
+              alert(JSON.stringify(values, null, 2)); // funkcja uruchamiana przy submicie formularza
+            }}
+          >
 
-export default App;
+            {({ handleSubmit, errors, touched }) => ( // przekazywanie propsów z formika do komponentu
+            <form onSubmit={handleSubmit}>
+              <VStack spacing={4} align="flex-start">
+              <FormControl isInvalid={!!errors.name && touched.name}>
+                  <FormLabel htmlFor="name">Imię</FormLabel>
+                  <Field
+                    as={Input}
+                    id="name"
+                    name="name"
+                    type="text"
+                    variant="filled"
+                  />
+                  <FormErrorMessage>{errors.name}</FormErrorMessage>
+                </FormControl>
+
+                <FormControl isInvalid={!!errors.age && touched.age}>
+                  <FormLabel htmlFor="age">Wiek</FormLabel>
+                  <Field
+                      as={Input}
+                    id="age"
+                    name="age"
+                    type="number"
+                    variant="filled"
+                  />
+                  <FormErrorMessage>{errors.age}</FormErrorMessage>
+                </FormControl>
+
+                <FormControl isInvalid={!!errors.volunteer && touched.volunteer}>
+                  <FormLabel htmlFor="volunteer">Czy wolontariusz?</FormLabel>
+                  <Field
+                      as={Input}
+                    id="volunteer"
+                    name="volunteer"
+                    type="text"
+                    variant="filled"
+                  />
+                  <FormErrorMessage>{errors.age}</FormErrorMessage>
+                </FormControl>
+
+                <FormControl isInvalid={!!errors.photo && touched.photo}>
+                  <FormLabel htmlFor="photo">Zdjęcie</FormLabel>
+                  <Field
+                      as={Input}
+                    id="photo"
+                    name="photo"
+                    type="file"
+                    variant="filled"
+                  />
+                  <FormErrorMessage>{errors.photo}</FormErrorMessage>
+                </FormControl>
+
+                <FormControl isInvalid={!!errors.zip && touched.zip}>
+                  <FormLabel htmlFor="zip">Kod pocztowy</FormLabel>
+                  <Field
+                      as={Input}
+                    id="zip"
+                    name="zip"
+                    type="text"
+                    variant="filled"
+                  />
+                  <FormErrorMessage>{errors.zip}</FormErrorMessage>
+                </FormControl>
+
+                <FormControl isInvalid={!!errors.telephone && touched.telephone}>
+                  <FormLabel htmlFor="telephone">Telefon</FormLabel>
+                  <Field
+                      as={Input}
+                    id="telephone"
+                    name="telephone"
+                    type="text"
+                    variant="filled"
+                  />
+                  <FormErrorMessage>{errors.telephone}</FormErrorMessage>
+                </FormControl>
+
+
+
+
+
+
+
+                <Button type="submit" colorScheme="purple" width="full">
+                  Zamów
+                </Button>
+
+              </VStack>
+            </form>
+            )}
+          </Formik>
+        </Box>
+      </Flex>
+    );
+  
+}
